1e31baa95bed489adaa70590e3bd30eb5f51d6f1
abdelkader <bouzomitaabdelkader@gmail.com>
2023-12-02 16:39:18 +0100

    "Successfully complete Day 2 problem."

--- day2/part1.js
+++ day2/part1.js
@@ -1,1 +1,33 @@
-//comming soon
+import fs from 'fs';
+const sumOfPossibleGameIds = (path) => {
+  const lines = fs.readFileSync(path, 'utf-8').split('\n');
+  let sumIds = 0;
+  lines.map((line) =>
+  {
+    if (isGamePossible(line)) {
+      sumIds += Number(line.substring(5, line.indexOf(':')));
+    }
+  });
+  return sumIds
+};
+const isGamePossible = (game) =>
+{
+  let res = true;
+  const gameSetList = game.substring(game.indexOf(':') + 1).trim().split(';')
+  gameSetList.map((gameSet) =>
+  {
+    gameSet.split(',').map((el) =>
+    {
+      if (el.includes('red')&&parseInt(el) > 12){
+        res=false
+      }
+      else if (el.includes('green') && parseInt(el) > 13) {
+        res=false
+      } else if (el.includes('blue') && parseInt(el) > 14) {
+        res=false
+      }
+    })
+  })
+  return res
+};
+console.log(sumOfPossibleGameIds('./input.txt'));
